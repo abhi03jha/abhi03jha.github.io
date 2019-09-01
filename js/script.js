dfa59ab0432a76801fd855a5a67ca0caf2e93c49
abhi03jha <54773052+abhi03jha@users.noreply.github.com>
2019-09-01 20:41:09 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function clickHome(){
+    document.querySelector('#mainHead').scrollIntoView({
+        behavior: "smooth"
+    });
+}
+function clickAbout() {
+    document.querySelector('#about').scrollIntoView({
+        behavior: "smooth"
+    });
+}
+function clickSpec() {
+    document.querySelector('#specials').scrollIntoView({
+        behavior: "smooth"
+    });
+}
+function clickServ() {
+    document.querySelector('#services').scrollIntoView({
+        behavior: "smooth"
+    });
+}
+function clickTeam() {
+    document.querySelector('#team').scrollIntoView({
+        behavior: "smooth"
+    });
+}
+function clickCont() {
+    document.querySelector('#contact').scrollIntoView({
+        behavior: "smooth"
+    });
+}
